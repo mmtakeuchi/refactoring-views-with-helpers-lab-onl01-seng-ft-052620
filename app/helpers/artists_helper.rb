37115b456846815433ca2470e28dc41126bb008e
mmtakeuchi <mikemtaki@gmail.com>
2020-08-07 11:13:27 -0700

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,5 @@
 module ArtistsHelper
+    def display_artist(song)
+        link_to song.artist ? song.artist.name : "Add Artist", song.artist ? artist_path(song.artist) : "/songs/#{song.id}/edit"
+    end
 end
